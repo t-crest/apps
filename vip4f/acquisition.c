323399f5447c3881f353aba9e9e8c6e856bd0cf1
Mathieu Jan <Mathieu.Jan@cea.fr>
2019-03-21 10:26:16 -0700

    Some cleanup to prepare trace comparison between OASIS/Pharos version and Patmos version

--- vip4f/acquisition.c
+++ vip4f/acquisition.c
@@ -22,6 +22,7 @@ void display(struct vip4f_t *vip4f, int current_time)
   if (local_counter_trs <= 0) local_counter_trs = 0;
   
   //  printf("Time=%d\n", current_time);
+  // TODO: here c should be the local counter from ARGA! (see acquisition.psy)
   printf("ARGA/c=%ld, I1=%d, I2=%d, I3=%d, Io=%d\n", local_counter,
 	 (int) vip4f->DataBufferI[local_counter % D_TRS_NB_ECH_FILTRE][D_ACQ_VOIE_I1],
 	 (int) vip4f->DataBufferI[local_counter % D_TRS_NB_ECH_FILTRE][D_ACQ_VOIE_I2], 
@@ -33,17 +34,23 @@ void display(struct vip4f_t *vip4f, int current_time)
 	 (int) vip4f->V_TRS_CumulRms[D_ACQ_VOIE_I3],
 	 vip4f->V_TRS_CumulRms2[D_ACQ_VOIE_I1], vip4f->V_TRS_CumulRms2[D_ACQ_VOIE_I2],
 	 vip4f->V_TRS_CumulRms2[D_ACQ_VOIE_I3]);
+  // TODO: this is part of the trace from AgRMS in mesures.psy
+  // but nothing is done with these values ...
   /* printf("RMS/I1=%d, I2=%d, I3=%d, Io=%d, I1RMS=%d, I2RMS=%d, I3RMS=%d\n",  */
   /* 	 vip4f->I[D_ACQ_VOIE_I1], vip4f->I[D_ACQ_VOIE_I2], */
   /* 	 vip4f->I[D_ACQ_VOIE_I3], vip4f->I[D_ACQ_VOIE_Io], */
   /* 	 (int) vip4f->RMS[D_ACQ_VOIE_I1], (int) vip4f->RMS[D_ACQ_VOIE_I2], */
   /* 	 (int) vip4f->RMS[D_ACQ_VOIE_I3]); */
-  
+
+  // TODO: counter should be removed for the comparison with orignal code
   printf("MOY/c=%ld, I1=%d, I2=%d, I3=%d, Io=%d\n", local_counter_trs,
 	 vip4f->V_TRS_CumulFiltre[local_counter_trs % D_TRS_NB_BUF_I][D_ACQ_VOIE_I1],
 	 vip4f->V_TRS_CumulFiltre[local_counter_trs % D_TRS_NB_BUF_I][D_ACQ_VOIE_I2], 
 	 vip4f->V_TRS_CumulFiltre[local_counter_trs % D_TRS_NB_BUF_I][D_ACQ_VOIE_I3],
 	 vip4f->V_TRS_CumulFiltre[local_counter_trs % D_TRS_NB_BUF_I][D_ACQ_VOIE_Io]);
+  // TODO: this is part of the original trace
+  // (in the Patmos version it is grouped in a single task explaining, thus not shared
+  // and thus not part of the vip4f structure. This is why we comment out this
   /* printf("CRE/I1.S=%d, I2.S=%d, I3.S=%d\n",  */
   /*         vip4f->V_DETC[D_ACQ_VOIE_I1].S, vip4f->V_DETC[D_ACQ_VOIE_I2].S, */
   /* 	 vip4f->V_DETC[D_ACQ_VOIE_I3].S); */
@@ -56,6 +63,7 @@ void display(struct vip4f_t *vip4f, int current_time)
   /* 	 (unsigned long long)(vip4f->VS_Mod2[D_ACQ_VOIE_I2]),  */
   /* 	 (unsigned long long)(vip4f->VS_Mod2[D_ACQ_VOIE_I3]), */
   /* 	 (unsigned long long)(vip4f->VS_Mod2[D_ACQ_VOIE_Io])); */
+  // TODO: Crete Non filtre is missing! (see mesures.psy)
   /* printf("TRS/V_mod2Imax=%Lu\n", (unsigned long long)(vip4f->V_mod2Imax)); */
 
   // TODO: add display when protection is triggered
